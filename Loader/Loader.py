9e97cc3795b776319ab0bcb73b6acdf6d974f00c
MNGuenther <mg719@cam.ac.uk>
2017-01-30 14:10:03 +0000

    Included NGTS loader (ngtsio); re-structure Candidate class

--- Loader/Loader.py
+++ Loader/Loader.py
@@ -1,5 +1,8 @@
+
+import numpy as np
 import fitsio
-import os
+#import os
+
 
 #'Candidate' Class
 class Candidate(object):
@@ -16,13 +19,15 @@ class Candidate(object):
         observatory -- source of candidate. Accepted values are: [NGTS,Kepler,K2]
         label       -- known classification, if candidate in training set. None if not known.
         """
-        self.id = id   
+        self.id = id
         self.filepath = filepath
         self.obs = observatory 
-        self.lightcurve = LoadLightcurve(self.id,self.filepath,self.obs)
+        self.lightcurve = self.LoadLightcurve()
         self.label = label    
 
-    def LoadLightcurve(self.id,self.filepath,self.obs):
+
+
+    def LoadLightcurve(self):
         """
         Load lightcurve from set observatory.
         
@@ -31,9 +36,9 @@ class Candidate(object):
         """
         if self.obs=='NGTS':
             #self.field = os.path.split(filepath)[1][:11]
-            lc = NGTSload(self.id,self.filepath)
+            lc = self.NGTSload()
         elif self.obs=='Kepler' or self.obs=='K2':
-            lc = KepK2load(self.filepath)
+            lc = self.KepK2load()
         else:
             print 'Observatory not supported'
             
@@ -41,28 +46,55 @@ class Candidate(object):
         
     
     
-def NGTSload(id,filepath):
-
-
-
-def KepK2Load(infile,inputcol='PDCSAP_FLUX',inputerr='PDCSAP_FLUX_ERR'):
-    """
-    Loads a Kepler or K2 lightcurve, normalised and with NaNs removed.
+    def NGTSload(self):
+        '''
+        filepath = <root> + <ngts_version> + <fieldname_camera_year_ngts_version.fits>
+        '''
+        from ngtsio import ngtsio
+        fnames = {'nights':self.filepath, 'sysrem':None, 'bls':None, 'canvas':None}
+        dic = ngtsio.get( None, ['HJD','FLUX','FLUX_ERR'], obj_id=self.id, fnames=fnames )
+        
+        nan_zero_cut = np.isnan(dic['HJD']) | np.isnan(dic['FLUX']) | np.equal(dic['FLUX'], 0)    
+        norm = np.median(dic['FLUX'][~nan_zero_cut])
+        lc = {}
+        lc['time'] = dic['HJD'][~nan_zero_cut]
+        lc['flux'] = dic['FLUX'][~nan_zero_cut]/norm
+        lc['error'] = dic['FLUX_ERR'][~nan_zero_cut]/norm
+        del dic
+        return lc
+        
     
-    Inputs:
-    infile -- input filepath
     
-    Returns:
-    lc -- lightcurve as dict, with keys time, flux, error
-    """
-    dat = fitsio.FITS(infile)
-    time = dat[1].read('TIME')
-    flux = dat[1].read(inputcol)
-    err = dat[1].read(inputerr)
-    nancut = np.isnan(time) | np.isnan(flux)
-    norm = np.median(flux[~nancut])
-    lc = {}
-    lc['time'] = time[~nancut]
-    lc['flux'] = flux[~nancut]/norm
-    lc['error'] = err[~nancut]/norm
-    return lc
+    def KepK2Load(self,inputcol='PDCSAP_FLUX',inputerr='PDCSAP_FLUX_ERR'):
+        """
+        Loads a Kepler or K2 lightcurve, normalised and with NaNs removed.
+        
+        Inputs:
+        infile -- input filepath
+        
+        Returns:
+        lc -- lightcurve as dict, with keys time, flux, error
+        """
+        dat = fitsio.FITS(self.filepath)
+        time = dat[1].read('TIME')
+        flux = dat[1].read(inputcol)
+        err = dat[1].read(inputerr)
+        nancut = np.isnan(time) | np.isnan(flux)
+        norm = np.median(flux[~nancut])
+        lc = {}
+        lc['time'] = time[~nancut]
+        lc['flux'] = flux[~nancut]/norm
+        lc['error'] = err[~nancut]/norm
+        return lc
+
+
+
+
+def test():
+    can = Candidate('020047', '/wasp/scratch/TEST18/NG0409-1941_812_2016_TEST18.fits')
+    print can.lightcurve
+    
+    
+    
+if __name__ == '__main__':
+    test()
