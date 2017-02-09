082b2b541074ea755f15afc7a37de21c904be9b6
DJArmstrong <d.j.armstrong@warwick.ac.uk>
2017-02-09 14:54:38 +0100

    added utils file

--- Features/Featureset.py
+++ Features/Featureset.py
@@ -1,6 +1,7 @@
 import PeriodLS
 import numpy as np
 from scipy import interpolate,stats
+import utils
 
 class Featureset(object):
 
@@ -78,9 +79,9 @@ class Featureset(object):
         period = self.features['LSPeriod'][0,0]  #assumes most significant LS period hit, and that LSPeriod was set to return more than one (default returns 4)
      
         phaselc2P = np.zeros([len(lc['time']),2])
-        phaselc2P[:,0] = self.phasefold(lc['time'],period*2)
+        phaselc2P[:,0] = utils.phasefold(lc['time'],period*2)
         phaselc2P = phaselc2P[np.argsort(phaselc2P[:,0]),:] #now in phase order
-        binnedlc2P,binstd = self.BinPhaseLC(phaselc2P,64)
+        binnedlc2P,binstd = utils.BinPhaseLC(phaselc2P,64)
 
         minima = np.argmin(binnedlc2P[:,1])
         posssecondary = np.mod(np.abs(binnedlc2P[:,0]-np.mod(binnedlc2P[minima,0]+0.5,1.)),1.)
@@ -113,13 +114,13 @@ class Featureset(object):
     def NZeroCross(self,window=16):
         lc = self.target.lightcurve
         
-        time_cut, flux_cut = self.CutOutliers(lc['time'],lc['flux'])
+        time_cut, flux_cut = utils.CutOutliers(lc['time'],lc['flux'])
         
         #interpolate gaps
-        interp_time,interp_flux = self.FillGaps_Linear(time_cut,flux_cut)
+        interp_time,interp_flux = utils.FillGaps_Linear(time_cut,flux_cut)
         
         #Do smoothing
-        flux_smooth = self.MovingAverage(interp_flux,window)
+        flux_smooth = utils.MovingAverage(interp_flux,window)
         flux_smooth = flux_smooth[window/2:-window/2]
 
         norm = np.median(flux_smooth)
@@ -168,6 +169,9 @@ class Featureset(object):
         mednorm = np.median(flux)
         return 1.4826 * np.median(np.abs(flux - mednorm))
 
+    def RMS(self):
+        return np.sqrt(np.mean(np.power(self.target.lightcurve['flux']-np.median(self.target.lightcurve['flux']),2)))
+    
     def SPhot(self,k=5):
         """
         S_phot,k diagnostic (see Mathur et al 2014)
@@ -207,21 +211,10 @@ class Featureset(object):
             pointindex += 1
         
         sphot = np.array(sphot)
-        contrast = self.CalcContrast(sphot,np.std(lc['flux']))
+        contrast = utils.CalcContrast(sphot,np.std(lc['flux']))
         
         return {'SPhot_mean':np.mean(sphot),'SPhot_median':np.median(sphot),'SPhot_max':np.max(sphot),'SPhot_min':np.min(sphot),'Contrast':contrast,'k':k}
   
-    def CalcContrast(self,SPhotseg,SPhotglob):  #SPhotglob is std of whole lightcurve
-        """
-        Contrast (see Mathur et al 2014)
-        
-        Will be returned as part of SPhot.
-        """
-        nhigh = SPhotseg>=SPhotglob
-        SPhothigh = np.mean(SPhotseg[nhigh])
-        SPhotlow = np.mean(SPhotseg[~nhigh])
-        return SPhothigh/SPhotlow
-
 
    # def PontRedNoise(self,cut_outliers=False):
    #     lc = self.target.lightcurve
@@ -268,62 +261,18 @@ class Featureset(object):
             lc = self.target.lightcurve
     
         if cut_outliers:
-            time_cut, flux_cut = self.CutOutliers(lc['time'],lc['flux'])
+            time_cut, flux_cut = utils.CutOutliers(lc['time'],lc['flux'])
         else:
             flux_cut = lc['flux']
             time_cut = lc['time']
         
         #interpolate gaps
-        interp_time,interp_flux = self.FillGaps_Linear(time_cut,flux_cut)
+        interp_time,interp_flux = utils.FillGaps_Linear(time_cut,flux_cut)
     
         #Do smoothing
-        flux_smooth = self.MovingAverage(interp_flux,window)
+        flux_smooth = utils.MovingAverage(interp_flux,window)
         flux_smooth = flux_smooth[window/2:-window/2]
         interp_smooth = interpolate.interp1d(interp_time[window/2:-window/2],flux_smooth,kind='linear',fill_value='extrapolate')
 
         return np.std(flux_cut - interp_smooth(time_cut))
 
-    def CutOutliers(self,time,flux):
-        threshold = np.percentile(flux,[2,98])
-        mednorm = np.median(flux)
-        MAD_calc = 1.4826 * np.median(np.abs(flux - mednorm))
-        sigmathreshold = [5*MAD_calc + mednorm,mednorm - 5*MAD_calc]
-        if threshold[1] > sigmathreshold[1]:  #takes the least active cut option
-            cut = (flux<threshold[1])&(flux>threshold[0])
-        else:
-            cut = (flux<sigmathreshold[1])&(flux>sigmathreshold[0])
-
-        return time[cut],flux[cut]
-
-    def MovingAverage(self,interval, window_size): #careful with start and end. Also requires regular grid.
-        window= np.ones(int(window_size))/float(window_size)
-        return np.convolve(interval, window, 'same')
-
-    def FillGaps_Linear(self,time,flux):       
-        cadence = np.median(np.diff(time))
-        npoints = np.floor((time[-1]-time[0])/cadence)
-        interp_times = np.arange(npoints)*cadence + time[0]
-        if interp_times[-1] > time[-1]:
-            interp_times = interp_times[:-1]
-        interp_obj = interpolate.interp1d(time,flux,kind='linear')
-        interp_flux = interp_obj(interp_times)
-        return interp_times,interp_flux
-
-
-    def phasefold(self,time,per):
-        return np.mod(time,per)/per
-    
-    def BinPhaseLC(self,phaselc,nbins):
-        bin_edges = np.arange(nbins)/float(nbins)
-        bin_indices = np.digitize(phaselc[:,0],bin_edges) - 1
-        binnedlc = np.zeros([nbins,2])
-        binnedlc[:,0] = 1./nbins * 0.5 +bin_edges  #fixes phase of all bins - means ignoring locations of points in bin, but necessary for SOM mapping
-        binnedstds = np.zeros(nbins)
-        for bin in range(nbins):
-            if np.sum(bin_indices==bin) > 0:
-                binnedlc[bin,1] = np.mean(phaselc[bin_indices==bin,1])  #doesn't make use of sorted phase array, could probably be faster?
-                binnedstds[bin] = np.std(phaselc[bin_indices==bin,1])
-            else:
-                binnedlc[bin,1] = np.mean(phaselc[:,1])  #bit awkward this, but only alternative is to interpolate?
-                binnedstds[bin] = np.std(phaselc[:,1])
-        return binnedlc,binnedstds
