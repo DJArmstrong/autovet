29b7f70cd6ad43f160c4f0647cd7c6981fe38a58
MNGuenther <mg719@cam.ac.uk>
2017-08-22 20:50:53 +0100

    ngtsio updates for CYCLE1706

--- test.py
+++ test.py
@@ -50,14 +50,14 @@ def test(case):
 #        fieldname = 'NG0537-3056'
 #        ngts_version = 'TEST18'
 #
-        obj_id = '006328'
-        fieldname = 'NG1421+0000'
-        ngts_version = 'TEST18'
+#        obj_id = '006328'
+#        fieldname = 'NG1421+0000'
+#        ngts_version = 'TEST18'
 #
-#        obj_id = '019164'
-#        fieldname = 'NG0524-3056'
-#        ngts_version = 'CYCLE1706'
-#
+        obj_id = '019164'
+        fieldname = 'NG0524-3056'
+        ngts_version = 'CYCLE1706'
+
 #        obj_id = '022551'
 #        fieldname = 'NG0524-3056'
 #        ngts_version = 'CYCLE1706'
